95591e78b3d3ce1c7c973df424299594e9eb3b40
nmningmei <semsociaty@gmail.com>
2021-09-05 03:03:12 +0200

    update

--- scripts/encoding_based_RSA_pipeline.py
+++ scripts/encoding_based_RSA_pipeline.py
@@ -11,7 +11,7 @@ Link:https://www.biorxiv.org/content/10.1101/2020.06.15.153247v2.full.pdf
 
 """
 
-import os
+import os,gc
 
 import numpy  as np
 import pandas as pd
@@ -20,44 +20,169 @@ from glob import glob
 
 from nilearn.input_data import NiftiMasker
 from nilearn.datasets import load_mni152_template,load_mni152_brain_mask
-from nilearn.image import concat_imgs
-
+from joblib import Parallel,delayed
 from sklearn import linear_model,preprocessing,metrics,model_selection as skms
 from sklearn.pipeline import make_pipeline
 
-from utils import load_event_files,load_computational_features
+from scipy.spatial             import distance
+from nibabel                   import load as load_fmri
+from nilearn.image             import new_img_like
+from brainiak.searchlight.searchlight import Searchlight
+from brainiak.searchlight.searchlight import Ball
 
-for item in glob('core.*'):
-    os.system(f'rm {item}')
-# parameters in the header
-sub                 = '*'
-model_name          = 'mobilenet'
-working_dir         = f'../results/Searchlight_standard/{sub}'
-event_dir           = f'../data/Searchlight/{sub}'
-mask_dir            = f'../data/masks_and_transformation_matrices/{sub}'
-whole_brain_data    = np.sort(glob(os.path.join(working_dir,'*.nii.gz')))
-events              = np.sort(glob(os.path.join(event_dir,'while_brain_bold_stacked.csv')))
-whole_brain_mask    = load_mni152_brain_mask()
-example_func        = load_mni152_template()
+from utils import (load_event_files,
+                   load_computational_features,
+                   groupby_average,
+                   searchlight_function_unit,
+                   feature_normalize)
 
-# prepare for loading the data
-masker              = NiftiMasker(whole_brain_mask,).fit()
-concat_BOLD         = concat_imgs(whole_brain_data) # for RSA
-df_events           = pd.concat([load_event_files(f) for f in events])
-BOLD_array          = masker.transform(concat_BOLD) # for encoding
-df_events['words']  = df_events['words'].apply(lambda x:x.lower())
-unique_words        = np.load('../data/unique_words/words.npy').astype(str)
-df_features         = load_computational_features(model_name)
-condition           = 'read'
+def score_func(y, y_pred,tol = 1e-2,func = np.mean,is_train = True):
+    temp        = metrics.r2_score(y,y_pred,multioutput = 'raw_values')
+    n_positive  = np.sum(temp > tol)
+    if n_positive > 0:
+        score   = func(temp[temp > tol])
+    else:
+        score   = 0
+    counter     = 0
+    the_number  = n_positive
+    while the_number > 0:
+        the_number = the_number // 10
+        counter += 1
+    if is_train:
+        return 0.5 * score + 0.5 * (n_positive / 10 ** counter)
+    else:
+        return score
 
-# leave one subject out cross validation
-idx_sub             = '123' # change here
-idx_test            = df_events['sub'] == idx_sub
-idx_train           = df_events['sub'] != idx_sub
+scorer = metrics.make_scorer(score_func, greater_is_better = True,)
 
+def _searchligh_RSA(input_image,
+                    computational_model_RDM,
+                    whole_brain_mask,
+                    sl_rad                          = 9,
+                    max_blk_edge                    = 9 - 1,
+                    shape                           = Ball,
+                    min_active_voxels_proportion    = 0,
+                    ):
+    """
+    This is function is defined here is because the complex environment
+    settings where Brainiak is installed
+    """
+    sl = Searchlight(sl_rad                         = sl_rad, 
+                     max_blk_edge                   = max_blk_edge, 
+                     shape                          = shape,
+                     min_active_voxels_proportion   = min_active_voxels_proportion,
+                     )
+    sl.distribute([np.asanyarray(input_image.dataobj)], 
+                   np.asanyarray(whole_brain_mask.dataobj) == 1)
+    sl.broadcast(computational_model_RDM)
+    # run searchlight algorithm
+    global_outputs = sl.run_searchlight(searchlight_function_unit,
+                                        pool_size = -1)
+    return global_outputs
 
-
-
+if __name__ == "__main__":
+    for item in glob('core.*'):
+        os.system(f'rm {item}')
+    # parameters in the header
+    sub                 = '*'
+    idx_sub             = '123' # change sub
+    condition           = 'read' # change condition
+    model_name          = 'mobilenet' # change model_name
+    n_jobs              = 6 # change n_jobs
+    alpha_max           = 5 # change alpha
+    radius              = 10
+    working_dir         = f'../results/Searchlight_standard/{sub}'
+    event_dir           = f'../data/Searchlight/{sub}'
+    mask_dir            = f'../data/masks_and_transformation_matrices/{sub}'
+    folder_name         = f'encoding_based_RSA_{radius}mm'
+    output_dir          = f'../results/{folder_name}'
+    whole_brain_data    = np.sort(glob(os.path.join(working_dir,'*.nii.gz')))
+    events              = np.sort(glob(os.path.join(event_dir,'while_brain_bold_stacked.csv')))
+    whole_brain_mask    = load_mni152_brain_mask()
+    example_func        = load_mni152_template()
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    
+    # prepare for loading the data
+    masker              = NiftiMasker(whole_brain_mask,).fit()
+    df_events           = pd.concat([load_event_files(f) for f in events])
+    res = Parallel(n_jobs = -1,verbose = 1,)(delayed(masker.transform)(**{
+            'imgs':item}) for item in whole_brain_data)
+    BOLD_array          = np.concatenate(res) # for encoding
+    del res
+    df_events['words']  = df_events['words'].apply(lambda x:x.lower())
+    unique_words        = np.load('../data/unique_words/words.npy').astype(str)
+    df_features         = load_computational_features(model_name)
+    
+    # pick condition
+    idx_condition       = df_events['context'] == condition
+    brain_features      = BOLD_array[idx_condition]
+    df_data             = df_events[idx_condition]
+    del BOLD_array
+    
+    # leave one subject out cross validation
+    idx_test            = df_data['sub'] == idx_sub
+    idx_train           = df_data['sub'] != idx_sub
+    
+    array_train         = brain_features[idx_train]
+    df_data_train       = df_data[idx_train]
+    features_train      = np.array([df_features[item] for item in df_data_train['words'].values])
+    groups_train        = df_data_train['sub'].values
+    array_test          = brain_features[idx_test]
+    df_data_test        = df_data[idx_test]
+    features_test       = np.array([df_features[item] for item in df_data_test['words'].values])
+    del brain_features
+    
+    cv                  = skms.LeaveOneGroupOut()
+    linear_reg          = linear_model.Ridge(fit_intercept  = True,
+                                             normalize      = True,
+                                             random_state   = 12345,
+                                             )
+    reg                 = make_pipeline(preprocessing.MinMaxScaler((-1,1)),
+                                        linear_reg,
+                                        )
+    scaler_brain        = preprocessing.MinMaxScaler((-1,1)).fit(array_train)
+    array_train_normalized = scaler_brain.transform(array_train)
+    param_grid          = {'ridge__alpha':np.logspace(2,alpha_max,alpha_max - 2 + 1)}
+    gc.collect()
+    grid_search         = skms.GridSearchCV(reg,
+                                            param_grid,
+                                            scoring = scorer,
+                                            cv      = skms.LeaveOneGroupOut(),
+                                            verbose = 1,
+                                            n_jobs  = n_jobs,
+                                    )
+    grid_search.fit(features_train,array_train_normalized,groups_train)
+    gc.collect()
+    
+    array_test_pred = grid_search.predict(features_test)
+    
+    temp,df_average = groupby_average([array_test_pred,features_test],
+                                      df_data_test.reset_index(drop = True),
+                                      groupby = ['words'])
+    
+    BOLD_average    = temp[0]
+    features        = temp[1]
+    
+    # normalize the model features
+    features        = feature_normalize(features)
+    # RDMs of the model features
+    RDM             = distance.pdist(features,'correlation')
+    BOLD_average    = masker.inverse_transform(BOLD_average)
+    
+    # perform RSA on the brain
+    gc.collect()
+    res             = _searchligh_RSA(BOLD_average,
+                                      RDM,
+                                      whole_brain_mask,
+                                      sl_rad = radius,
+                                      )
+    gc.collect()
+    
+    res  = new_img_like(example_func,np.array(res,  dtype = np.float),)
+    
+    # save
+    res.to_filename(os.path.join(output_dir, f'{idx_sub}_{condition}_{model_name}.nii.gz'))
 
 
 
--- scripts/encoding_based_RSA_pipeline_modify_and_save.py
+++ scripts/encoding_based_RSA_pipeline_modify_and_save.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Aug 29 02:44:20 2021
+Created on Sat Sep  4 14:57:40 2021
 
 @author: nmei
 """
@@ -10,21 +10,23 @@ import os
 import numpy as np
 import pandas as pd
 from shutil import rmtree,copyfile
+from itertools import product
 
-template = '2.1.randomise on the RSA maps.py'
+template = 'encoding_based_RSA_pipeline.py'
 working_dir = '../data/Searchlight'
 subjects = os.listdir(working_dir)
-cv_model_names = ['vgg19','mobilenet','resnet50']
-w2v_model_names = ['fasttext','glove','word2vec']
+model_names = ['vgg19','mobilenet','resnet50','fasttext','glove','word2vec']
 conditions = ['read','reenact']
+df_iteration = pd.DataFrame(list(product(subjects,model_names,conditions)),
+                            columns = ['sub','model_name','condition'])
 
 node = 1
 core = 16
-mem = 4 * node * core
+mem = 6 * node * core
 cput = 24 * node * core
 
 #############
-scripts_folder = 'randomise'
+scripts_folder = 'ENRSA'
 if not os.path.exists(scripts_folder):
     os.mkdir(scripts_folder)
 else:
@@ -38,66 +40,55 @@ with open ('../.gitignore','r') as f:
     f.close()
 if np.sum(temp) == 0:
     with open('../.gitignore','a') as f:
-        line = f'#bash folder\nscripts/{scripts_folder}\n'
+        line = f'# bash folder\nscripts/{scripts_folder}\n'
         f.write(line)
         f.close()
-from shutil import copyfile
+
 copyfile('utils.py',f'{scripts_folder}/utils.py')
 
 collections = []
-for ii in range(12):
-    new_scripts_name = os.path.join(scripts_folder,template.replace('.py',f'_{ii}.py'))
+for ii,row in df_iteration.iterrows():
+    sub,model_name,condition = row
+    new_scripts_name = os.path.join(scripts_folder,template.replace('.py',f'_{sub}_{model_name}_{condition}.py'))
     with open(new_scripts_name,'w') as new_file:
         with open(template,'r') as old_file:
             for line in old_file:
                 if "../" in line:
                     line = line.replace("../","../../")
-                elif 'change idx' in line:
-                    line = f'idx = {ii}\n'
+                elif 'change sub' in line:
+                    line = f'    idx_sub             = "{sub}"\n'
+                elif 'change condition' in line:
+                    line = f'    condition           = "{condition}"\n'
+                elif 'change model_name' in line:
+                    line = f'    model_name          = "{model_name}"\n'
+                elif "change n_jobs" in line:
+                    line = '    n_jobs              = -1\n'
+                elif "change alpha_max" in line:
+                    line = '    alpha_max           = 20\n'
                 new_file.write(line)
             old_file.close()
         new_file.close()
-    new_batch_script_name = os.path.join(scripts_folder,f'RSA{ii}')
-#    content = f"""#!/bin/bash
-##$ -q long.q
-##$ -N Ran{ii}
-##$ -o outputs/out_{ii}.txt
-##$ -e outputs/err_{ii}.txt
-##$ -cwd
-##$ -m be
-##$ -M nmei@bcbl.eu
-##$ -S /bin/bash
-#
-#pwd
-#echo "randomise map {ii}"
-#module load python/python3.6 fsl/6.0.0
-#
-#python "{new_scripts_name.split('/')[-1]}"
-#"""
-
+    new_batch_script_name = os.path.join(scripts_folder,f'enRSA{ii}')
     content = f"""#!/bin/bash
 #PBS -q bcbl
 #PBS -l nodes={node}:ppn={core}
 #PBS -l mem={mem}gb
 #PBS -l cput={cput}:00:00
-#PBS -N RSA{ii+1}
+#PBS -N enRSA{ii}
 #PBS -o outputs/out_{ii}.txt
 #PBS -e outputs/err_{ii}.txt
-
-module load FSL/6.0.0-foss-2018b
+cd $PBS_O_WORKDIR
 export PATH="/scratch/ningmei/anaconda3/bin:/scratch/ningmei/anaconda3/condabin:$PATH"
 source activate keras-2.1.6_tensorflow-2.0.0
-cd $PBS_O_WORKDIR
-
 pwd
 echo {new_scripts_name.split('/')[-1]}
 python "{new_scripts_name.split('/')[-1]}"
-    """
+"""
     print(content)
     with open(new_batch_script_name,'w') as f:
         f.write(content)
         f.close()
-    collections.append(f"qsub RSA{ii}")
+    collections.append(f"qsub enRSA{ii}")
 
 with open(f'{scripts_folder}/qsub_jobs.py','w') as f:
     f.write("""import os\nimport time""")
@@ -107,5 +98,5 @@ with open(f'{scripts_folder}/qsub_jobs.py','a') as f:
         if ii == 0:
             f.write(f'\nos.system("{line}")\n')
         else:
-            f.write(f'time.sleep(3)\nos.system("{line}")\n')
+            f.write(f'time.sleep(.3)\nos.system("{line}")\n')
     f.close()
